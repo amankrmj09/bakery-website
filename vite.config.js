7372a5ac7baa13f9286bc1f98986fb8e93322fbc
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-28 22:42:22 +0530

    chore: update dependencies and vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,35 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(), 
+        tailwindcss(),
+        VitePWA({
+          registerType: 'autoUpdate',
+          workbox: {
+            runtimeCaching: [
+              {
+                urlPattern: ({ request }) => request.destination === 'image',
+                handler: 'CacheFirst',
+                options: {
+                  cacheName: 'images-cache',
+                  expiration: {
+                    maxEntries: 100,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                  },
+                  cacheableResponse: {
+                    statuses: [0, 200]
+                  }
+                }
+              }
+            ]
+          }
+        })
+    ],
     server: {
         port: 3001, // Admin is on 3000
         open: true,
