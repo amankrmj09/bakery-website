793e76f8c965642e0b84baf645b57e618f7cf7f7
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-26 10:15:27 +0530

    feat(app): integrate ScrollToTop component in App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from './components/layout/ScrollToTop';
 
 import { Toaster } from 'sonner';
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Toaster richColors position="top-center" />
       <AppRoutes />
     </>
