761abe8f91024fb8a8e9dc6575caa2ec00b8d006
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-26 10:18:19 +0530

    feat(addresses): dispatch fetchUserAddresses on component mount in MyAddresses

--- src/features/user/components/MyAddresses.jsx
+++ src/features/user/components/MyAddresses.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addAddress, updateAddress, deleteAddress, setDefaultAddress, clearAddressError } from '../redux/addressSlice';
+import { fetchUserAddresses, addAddress, updateAddress, deleteAddress, setDefaultAddress, clearAddressError } from '../redux/addressSlice';
 import { LuMapPin as MapPin, LuPlus as Plus, LuTrash2 as Trash, LuPencil as Edit, LuCheck as CheckCircle2, LuLoader as Loader2 } from 'react-icons/lu';
 import { toast } from 'sonner';
 
 export default function MyAddresses() {
   const dispatch = useDispatch();
   const { addresses, loading } = useSelector((state) => state.address);
+
+  useEffect(() => {
+    dispatch(fetchUserAddresses());
+  }, [dispatch]);
+
   const [isAdding, setIsAdding] = useState(false);
   const [editingId, setEditingId] = useState(null);
   
